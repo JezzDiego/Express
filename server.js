8e8da83ca4c0ab9194145f6951766dfaca37f451
JezzDiego <oliveirajesse50@gmail.com>
2022-05-06 01:47:46 -0300

    initial-setup

---
+++ server.js
@@ -0,0 +1,38 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const app = express();
+
+const indexRouter = require("./routes/index");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    family: 4,
+  })
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch((err) => {
+    console.log("Not Connected to Database ERROR! ", err);
+  });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => console.log("Connected to MongoDB"));
+
+app.use("/", indexRouter);
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
