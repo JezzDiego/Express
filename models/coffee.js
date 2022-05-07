5012e017a959e01aaf27a19d79dd32242feb0686
JezzDiego <oliveirajesse50@gmail.com>
2022-05-07 01:38:26 -0300

    feat: crate coffee form and errors messages

---
+++ models/coffee.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const coffeeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Coffee", coffeeSchema);
--- routes/coffees.js
+++ routes/coffees.js
@@ -1,16 +1,31 @@
 const express = require("express");
+const { render } = require("express/lib/response");
 const router = express.Router();
+const Coffee = require("../models/coffee");
 
 router.get("/", (req, res) => {
   res.render("coffees/index");
 });
 
 router.get("/new", (req, res) => {
-  res.render("coffees/new");
+  res.render("coffees/new", { coffee: new Coffee() });
 });
 
 router.post("/", (req, res) => {
-  res.send("Create");
+  const coffee = new Coffee({
+    name: req.body.name,
+  });
+  coffee.save((err, newCoffee) => {
+    if (err) {
+      res.render("coffees/new", {
+        coffee: coffee,
+        errorMessage: "Error creating coffee",
+      });
+    } else {
+      // res.redirect(`coffees/${newCoffee.id}`);
+      res.redirect("coffees");
+    }
+  });
 });
 
 module.exports = router;
